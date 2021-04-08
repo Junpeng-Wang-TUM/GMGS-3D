3e80bd40e3bb21c797d50b358402072bc09068fd
Junpeng-Wang-TUM <47067711+Junpeng-Wang-TUM@users.noreply.github.com>
2021-04-08 17:38:37 +0200

    Update 1

--- src/Main.m
+++ src/Main.m
@@ -21,14 +21,14 @@ triFaceModelFile = '../data/kitten.ply';
 % loadingCond_ = load('../data/demo_BC_kitten_64_loadingVec.dat');
 %%--------------------------------------------------------------------------------------------------
 %% Exp. 1.2 resolution = 128;
-% finestResolutionControl_ = 128;
-% fixingCond_ = load('../data/demo_BC_kitten_128_fixedNodes.dat');
-% loadingCond_ = load('../data/demo_BC_kitten_128_loadingVec.dat');
+finestResolutionControl_ = 128;
+fixingCond_ = load('../data/demo_BC_kitten_128_fixedNodes.dat');
+loadingCond_ = load('../data/demo_BC_kitten_128_loadingVec.dat');
 %%--------------------------------------------------------------------------------------------------
 %% Exp. 1.3 resolution = 256;
-finestResolutionControl_ = 256;
-fixingCond_ = load('../data/demo_BC_kitten_256_fixedNodes.dat');
-loadingCond_ = load('../data/demo_BC_kitten_256_loadingVec.dat');
+% finestResolutionControl_ = 256;
+% fixingCond_ = load('../data/demo_BC_kitten_256_fixedNodes.dat');
+% loadingCond_ = load('../data/demo_BC_kitten_256_loadingVec.dat');
 %%--------------------------------------------------------------------------------------------------
 %% Exp. 1.4 resolution = 512;
 % finestResolutionControl_ = 512;
@@ -73,10 +73,18 @@ loadingCond_ = load('../data/demo_BC_kitten_256_loadingVec.dat');
 tStart = tic;
 voxelizedVolume_ = CreateVoxilizedModel(triFaceModelFile, finestResolutionControl_);
 DiscretizeDesignDomain();
-disp(['Creating the geometrical model costs: ' sprintf('%10.3g',toc(tStart)) 's']);
+disp(['Creating the Geometrical Model Costs: ' sprintf('%10.3g',toc(tStart)) 's']);
 
 %%3. Applying for boundary condition
 ShowFEAmodel();
+%% For Specific Use of This Program:
+%% 1) Go to "GlobalVariables.m" for Basic Settings, e.g., Material Properties and Linear System Solver-related Parameters 
+%% 2) Input the Triangular Surface Mesh ("triFaceModelFile") and Target Resolution ("finestResolutionControl_")
+%% 3) Interactively Applying for Boundary Conditions via the Variable "boundaryNodeCoords" (Boundary Node Coordinates) and Functions like
+%%													 "./common/PickNode.m", 
+%%													 "./common/InitializeFixingCond.m"
+%%													 "./common/InitializeLoadingCond.m", etc.
+%% boundaryNodeCoords = nodeCoords_(meshHierarchy_(1).solidNodesMapVec(meshHierarchy_(1).nodesOnBoundary),:);
 ApplyBoundaryCondition();
 
 %%4. Initializing Solver
@@ -89,6 +97,6 @@ tStart = tic;
 U_ = CG_solver(@AtX, @Vcycle, F_, tol_, maxIT_, 'printP_ON');
 disp(['Solving Linear System Costs: ' sprintf('%10.3g',toc(tStart)) 's']);	
 
-%%5. Show deformation
+%%5. Show Deformation
 totDis = vecnorm(reshape(U_,3,numel(U_)/3)',2,2);
 VisScalarField(totDis, 0);
