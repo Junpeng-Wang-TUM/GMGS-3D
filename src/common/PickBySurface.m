a1ef968da796f3219960794844efd1c15c91e184
Junpeng-Wang-TUM <47067711+Junpeng-Wang-TUM@users.noreply.github.com>
2021-05-02 23:45:01 +0200

    update 1
    Extend function "PickBySurface(varargin)"

--- src/common/PickBySurface.m
+++ src/common/PickBySurface.m
@@ -1,4 +1,4 @@
-function PickByLine(constantDir)
+function PickBySurface(constantDir, opt, varargin)
 	global boundaryNodeCoords_;
 	global PickedNodeCache_;
 	global hdPickedNode_;
@@ -8,19 +8,34 @@ function PickByLine(constantDir)
 	if isempty(info_struct)
 		warning('No Cursor Mode Available!'); return;
 	end
+	if ~(1==opt || 0==opt || -1==opt), warning('Wrong Input!'); return; end
+	if 0==opt
+		if 2==nargin, surfRelaxFactor = 0; else, surfRelaxFactor = varargin{1}; end
+	end	
 	tarNode = info_struct.Position;
 	numPickedNode = size(PickedNodeCache_,1);
 	hold on; 
 	if 1~=length(constantDir), error('Wrongly Defined Line Direction!'); end
 	switch constantDir
 		case 'X'
-			nodesOnLine = find(boundaryNodeCoords_(:,1)==tarNode(1));
+			refNodePool = boundaryNodeCoords_(:,1);
+			refPos = tarNode(1);				
 		case 'Y'
-			nodesOnLine = find(boundaryNodeCoords_(:,2)==tarNode(2));
+			refNodePool = boundaryNodeCoords_(:,2);
+			refPos = tarNode(2);				
 		case 'Z'
-			nodesOnLine = find(boundaryNodeCoords_(:,3)==tarNode(3));				
+			refNodePool = boundaryNodeCoords_(:,3);
+			refPos = tarNode(3);				
 		otherwise
 			error('Wrongly Defined Line Direction!');
+	end
+	switch opt
+		case 1
+			nodesOnLine = find(refNodePool>=refPos);
+		case 0
+			nodesOnLine = find(abs(refNodePool-refPos)<=surfRelaxFactor);
+		case -1
+			nodesOnLine = find(refNodePool<=refPos);
 	end	
 	hdPickedNode_(end+1) = plot3(boundaryNodeCoords_(nodesOnLine,1), boundaryNodeCoords_(nodesOnLine,2), ...
 		boundaryNodeCoords_(nodesOnLine,3), 'xr', 'LineWidth', 2, 'MarkerSize', 10);
