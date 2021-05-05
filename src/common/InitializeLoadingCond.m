70f2761c394074daa9e593a3dd67f54698a9e9f5
Junpeng-Wang-TUM <47067711+Junpeng-Wang-TUM@users.noreply.github.com>
2021-05-05 22:24:12 +0200

    update 1

--- src/common/InitializeLoadingCond.m
+++ src/common/InitializeLoadingCond.m
@@ -3,7 +3,7 @@ function InitializeLoadingCond(force)
 	global loadingCond_;
 	global PickedNodeCache_;
 	
-	if isempty(PickedNodeCache_), warning('There is no node selected!'); end
+	if isempty(PickedNodeCache_), warning('There is no node selected!'); return;  end
 	PickedNodeCache_ = unique(PickedNodeCache_);
 	numTarNodes = length(PickedNodeCache_);
 	iLoadingVec = [double(meshHierarchy_(1).nodesOnBoundary(PickedNodeCache_)) repmat(force/numTarNodes, numTarNodes, 1)];
